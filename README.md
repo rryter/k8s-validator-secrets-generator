243abeb0364a3f4d44293e67c27dea8693485e5f
Reto Ryter <reto@twy.gmbh>
2021-10-22 09:16:34 +0000

    Revert "chore(simplify): skip deposit cli"
    This reverts commit e4774995824e9763dddc2255d4eb8f084bb4a2f7.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Usage
 
-- Step1: Edit lines 10 - 12 accordingly
+- Step1: Edit lines 10 - 13 accordingly
 - Step2: `python3 helper/cli.py`
 - Step3: Happy Dance.
--- helper/cli.py
+++ helper/cli.py
@@ -7,27 +7,35 @@ import glob
 @click.command()
 def main():
 
-    network = 'l15-staging'
-    amountOfNodes = 8
-    namespace = "staging"
+    network = 'l15-testnet-dev'
+    amountOfNodes = 5
+    namespace = "dev"
+    depositCli = "https://github.com/lukso-network/network-deposit-cli/releases/download/v1.2.4-l15/lukso-deposit-cli-8cf72bf-linux-amd64.tar.gz"
+    depositCliName = depositCli.split('/')[-1]
+    os.mkdir(network)
     os.mkdir(network + "/sealed-secrets")
 
+    os.system("wget " + depositCli + " >/dev/null")
+    os.system("tar -zxf "+depositCliName + " >/dev/null")
+    os.system("mv " + depositCliName.replace(".tar.gz", "") + " lukso-deposit-cli >/dev/null")
+    os.system("rm " + depositCliName + " >/dev/null")
     os.system("kubeseal --fetch-cert --controller-name=sealed-secrets --controller-namespace=sealed-secrets > pub-sealed-secrets.pem -n sealed-secrets")
 
+    print("mv " + depositCliName.replace(".tar.gz", "") + " lukso-deposit-cli")
     for x in range(amountOfNodes):
         print("Generating sealed keys for node " + str(x))
-        # mnemo = Mnemonic("english")
-        # words = mnemo.generate(strength=256)
+        mnemo = Mnemonic("english")
+        words = mnemo.generate(strength=256)
 
-        folder = network +"/wallet-" + str(x)
-        # os.mkdir(folder)
-        # f = open(folder + "/mnemonic", "x")
-        # f.write(words)
+        folder = network +"/node-" + str(x)
+        os.mkdir(folder)
+        f = open(folder + "/mnemonic", "x")
+        f.write(words)
 
-        # os.system('./lukso-deposit-cli/deposit existing-mnemonic --folder '+ folder +' --num_validators 5 --validator_start_index 5 --keystore_password 12345678 --chain '+network+' --mnemonic "' + words + '" >/dev/null'),
+        os.system('./lukso-deposit-cli/deposit existing-mnemonic --folder '+ folder +' --num_validators 5 --validator_start_index 5 --keystore_password 12345678 --chain '+network+' --mnemonic "' + words + '" >/dev/null'),
 
         commandStart = "kubectl create secret generic validator-keys-node-" + str(x) + " --from-file="
-        fromFile = ' --from-file='.join(glob.iglob(folder +'/direct/accounts/*.json'))
+        fromFile = ' --from-file='.join(glob.iglob(folder +'/validator_keys/*.json'))
         commandEnd = " --dry-run=client -o yaml > "+ folder + "/validator-keys-node-" + str(x) + ".yaml -n " + namespace
 
         os.system(commandStart + fromFile + commandEnd)

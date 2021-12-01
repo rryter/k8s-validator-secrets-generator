8cff54190183cd8938bbfb8d0c02ecb9fde9b626
Reto Ryter <reto@twy.gmbh>
2021-12-01 11:09:50 +0000

    chore(deposit-data): add concat function

--- helper/cli.py
+++ helper/cli.py
@@ -3,22 +3,22 @@ from mnemonic import Mnemonic
 import click
 import os
 import glob
+import json
 
 @click.command()
 def main():
 
-    network = 'l15-testnet-dev'
-    amountOfNodes = 5
+    network = 'l15-dev'
+    amountOfNodes = 8
     namespace = "dev"
-    depositCli = "https://github.com/lukso-network/network-deposit-cli/releases/download/v1.2.4-l15/lukso-deposit-cli-8cf72bf-linux-amd64.tar.gz"
+    depositCli = "https://github.com/lukso-network/network-deposit-cli/releases/download/v1.2.6-LUKSO/lukso-deposit-cli-Linux-x86_64"
     depositCliName = depositCli.split('/')[-1]
     os.mkdir(network)
     os.mkdir(network + "/sealed-secrets")
 
     os.system("wget " + depositCli + " >/dev/null")
-    os.system("tar -zxf "+depositCliName + " >/dev/null")
-    os.system("mv " + depositCliName.replace(".tar.gz", "") + " lukso-deposit-cli >/dev/null")
-    os.system("rm " + depositCliName + " >/dev/null")
+    os.system("mv " + depositCliName + " lukso-deposit-cli >/dev/null")
+    os.system("chmod +x " + depositCliName + " lukso-deposit-cli >/dev/null")
     os.system("kubeseal --fetch-cert --controller-name=sealed-secrets --controller-namespace=sealed-secrets > pub-sealed-secrets.pem -n sealed-secrets")
 
     print("mv " + depositCliName.replace(".tar.gz", "") + " lukso-deposit-cli")
@@ -32,7 +32,7 @@ def main():
         f = open(folder + "/mnemonic", "x")
         f.write(words)
 
-        os.system('./lukso-deposit-cli/deposit existing-mnemonic --folder '+ folder +' --num_validators 5 --validator_start_index 5 --keystore_password 12345678 --chain '+network+' --mnemonic "' + words + '" >/dev/null'),
+        os.system('./lukso-deposit-cli existing-mnemonic --folder '+ folder +' --num_validators 10 --validator_start_index 0 --keystore_password 12345678 --chain '+network+' --mnemonic "' + words + '" >/dev/null'),
 
         commandStart = "kubectl create secret generic validator-keys-node-" + str(x) + " --from-file="
         fromFile = ' --from-file='.join(glob.iglob(folder +'/validator_keys/*.json'))
@@ -41,5 +41,24 @@ def main():
         os.system(commandStart + fromFile + commandEnd)
         os.system("kubeseal --cert pub-sealed-secrets.pem --format yaml < "+ folder + "/validator-keys-node-" + str(x) + ".yaml > "+ network + "/sealed-secrets/validator-keys-node-" + str(x) + "-sealed.yaml -n " + namespace)
 
+def merge_JsonFiles(filename):
+        result = list()
+        for f1 in filename:
+            with open(f1, 'r') as infile:
+                result.extend(json.load(infile))
+
+        with open('deposit_data.json', 'w') as output_file:
+            json.dump(result, output_file)
+
+def concat():
+    depositDataFiles = []
+    # root_dir needs a trailing slash (i.e. /root/dir/)
+    for filename in glob.iglob('l15-dev/**/*deposit_data*.json', recursive=True):
+        depositDataFiles.append(filename)
+
+    depositDataFiles.sort()
+    merge_JsonFiles(depositDataFiles)
+
 if __name__ == "__main__":
-    main()
+    # main()
+    concat()
